b02958d9a6706e389aee0bf1e1a94b81bec23809
Yiming Cao <yiming@whimslab.io>
2025-09-03 23:56:51 -0700

    chore: fix telemetry token (#236)
    * chore: fix telemetry token
    
    * update
    
    * update
    
    * try again
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix

---
+++ packages/cli/scripts/post-build.ts
@@ -0,0 +1,20 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const token = process.env.TELEMETRY_TRACKING_TOKEN ?? '';
+
+if (!token) {
+    console.warn('TELEMETRY_TRACKING_TOKEN is not set.');
+}
+
+const filesToProcess = ['dist/index.js', 'dist/index.cjs'];
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+
+for (const file of filesToProcess) {
+    console.log(`Processing ${file} for telemetry token...`);
+    const filePath = path.join(_dirname, '..', file);
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const updatedContent = content.replace('<TELEMETRY_TRACKING_TOKEN>', token);
+    fs.writeFileSync(filePath, updatedContent, 'utf-8');
+}

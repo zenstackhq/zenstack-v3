01defdc1a8dac117e8e413f541452201779a9abf
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-09-03 22:54:21 -0700

    update

---
+++ packages/cli/scripts/post-build.ts
@@ -0,0 +1,17 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const token = process.env['TELEMETRY_TRACKING_TOKEN'] ?? '';
+
+console.log('TELEMETRY_TRACKING_TOKEN:', token);
+
+const filesToProcess = ['dist/index.js', 'dist/index.cjs'];
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+
+for (const file of filesToProcess) {
+    console.log(`Processing ${file} for telemetry token...`);
+    const content = fs.readFileSync(path.join(_dirname, '..', file), 'utf-8');
+    const updatedContent = content.replace('<TELEMETRY_TRACKING_TOKEN>', token);
+    fs.writeFileSync(file, updatedContent, 'utf-8');
+}
--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,5 +1,3 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
@@ -12,23 +10,4 @@ export default defineConfig({
     clean: true,
     dts: true,
     format: ['esm', 'cjs'],
-    env: {
-        TELEMETRY_TRACKING_TOKEN: process.env['TELEMETRY_TRACKING_TOKEN'] ?? '',
-    },
-    onSuccess: async () => {
-        console.log('TELEMETRY_TRACKING_TOKEN:', process.env['TELEMETRY_TRACKING_TOKEN']);
-        if (!process.env['TELEMETRY_TRACKING_TOKEN']) {
-            return;
-        }
-        const filesToProcess = ['dist/index.js', 'dist/index.cjs'];
-        for (const file of filesToProcess) {
-            console.log(`Processing ${file} for telemetry token...`);
-            const content = fs.readFileSync(path.join(__dirname, file), 'utf-8');
-            const updatedContent = content.replace(
-                '<TELEMETRY_TRACKING_TOKEN>',
-                process.env['TELEMETRY_TRACKING_TOKEN'],
-            );
-            fs.writeFileSync(file, updatedContent, 'utf-8');
-        }
-    },
 });

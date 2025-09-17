b17bf540a89163185d8f60ede14dd97f136ea26e
Yiming Cao <yiming@whimslab.io>
2025-09-17 12:44:55 -0700

    chore: migrate more v2 tests (#254)

--- packages/runtime/test/policy/migrated/auth.test.ts
+++ packages/runtime/test/policy/migrated/auth.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { createPolicyTestClient } from './utils';
+import { createPolicyTestClient } from '../utils';
 
 describe('auth() tests', () => {
     it('works with string id non-null test', async () => {
--- packages/runtime/test/policy/migrated/connect-disconnect.test.ts
+++ packages/runtime/test/policy/migrated/connect-disconnect.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { createPolicyTestClient } from './utils';
+import { createPolicyTestClient } from '../utils';
 
 describe('connect and disconnect tests', () => {
     const modelToMany = `

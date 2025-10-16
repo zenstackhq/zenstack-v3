258cee0f500b717dbf0d278c3ebe954954cd6c20
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-10-15 18:10:08 -0700

    fix: delegate count relation issue, default boolean value issue

--- packages/language/src/validators/attribute-application-validator.ts
+++ packages/language/src/validators/attribute-application-validator.ts
@@ -22,6 +22,7 @@ import {
 import {
     getAllAttributes,
     getStringLiteral,
+    hasAttribute,
     isAuthOrAuthMemberAccess,
     isBeforeInvocation,
     isCollectionPredicate,
@@ -364,6 +365,11 @@ function assignableToAttributeParam(arg: AttributeArg, param: AttributeParam, at
     if (dstType === 'ContextType') {
         // ContextType is inferred from the attribute's container's type
         if (isDataField(attr.$container)) {
+            // If the field is Typed JSON, and the param is @default, the argument must be a string
+            const dstIsTypedJson = hasAttribute(attr.$container, '@json');
+            if (dstIsTypedJson && param.default) {
+                return argResolvedType.decl === 'String';
+            }
             dstIsArray = attr.$container.type.array;
         }
     }

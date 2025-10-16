d3a560681bcb545db98b6d922416fdbf7e2c63d4
sanny-io <3054653+sanny-io@users.noreply.github.com>
2025-10-15 18:13:54 -0700

    fix: prisma plugin not respecting `zenstack.output` (#298)
    * fix: prisma plugin not respecting `zenstack.output`
    
    * chore: add test for #295
    
    ---------
    
    Co-authored-by: = <=>

--- packages/cli/src/plugins/prisma.ts
+++ packages/cli/src/plugins/prisma.ts
@@ -5,10 +5,10 @@ import path from 'node:path';
 const plugin: CliPlugin = {
     name: 'Prisma Schema Generator',
     statusText: 'Generating Prisma schema',
-    async generate({ model, schemaFile, defaultOutputPath, pluginOptions }) {
+    async generate({ model, defaultOutputPath, pluginOptions }) {
         let outFile = path.join(defaultOutputPath, 'schema.prisma');
         if (typeof pluginOptions['output'] === 'string') {
-            outFile = path.resolve(path.dirname(schemaFile), pluginOptions['output']);
+            outFile = path.resolve(defaultOutputPath, pluginOptions['output']);
             if (!fs.existsSync(path.dirname(outFile))) {
                 fs.mkdirSync(path.dirname(outFile), { recursive: true });
             }

dbe922f71f024edbc4ffbccfeba59abde2fba827
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-10-17 09:19:40 -0700

    chore: improve tests and fix typo

--- packages/testtools/src/utils.ts
+++ packages/testtools/src/utils.ts
@@ -1,7 +1,6 @@
 import { loadDocument } from '@zenstackhq/language';
-import path from 'node:path';
 
 export function loadDocumentWithPlugins(filePath: string) {
-    const pluginModelFiles = [path.resolve(__dirname, '../node_modules/@zenstackhq/plugin-policy/plugin.zmodel')];
+    const pluginModelFiles = [require.resolve('@zenstackhq/plugin-policy/plugin.zmodel')];
     return loadDocument(filePath, pluginModelFiles);
 }

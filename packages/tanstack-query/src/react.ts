5ee39a32e12c3cd68de59f0f1d5f36db52f534e0
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-11-02 08:53:38 -0800

    WIP

--- packages/tanstack-query/src/react.ts
+++ packages/tanstack-query/src/react.ts
@@ -5,51 +5,51 @@ import type {
     UseQueryOptions,
     UseQueryResult,
 } from '@tanstack/react-query';
+import { lowerCaseFirst } from '@zenstackhq/common-helpers';
 import type { CreateArgs, FindArgs, ModelResult, SelectSubset } from '@zenstackhq/orm';
 import type { GetModels, SchemaDef } from '@zenstackhq/orm/schema';
 
-export type toHooks<Schema extends SchemaDef> = {
-    [Model in GetModels<Schema> as Uncapitalize<Model>]: ToModelHooks<Schema, Model>;
+export type QueryHooks<Schema extends SchemaDef> = {
+    [Model in GetModels<Schema> as Uncapitalize<Model>]: ModelQueryHooks<Schema, Model>;
 };
 
-type ToModelHooks<Schema extends SchemaDef, Model extends GetModels<Schema>> = {
-    findMany<T extends FindArgs<Schema, Model, true>>(
+type ModelQueryHooks<Schema extends SchemaDef, Model extends GetModels<Schema>> = {
+    useFindMany<T extends FindArgs<Schema, Model, true>>(
         args?: SelectSubset<T, FindArgs<Schema, Model, true>>,
         options?: Omit<UseQueryOptions<ModelResult<Schema, Model, T>[]>, 'queryKey'>,
     ): UseQueryResult<ModelResult<Schema, Model, T>[]>;
 
-    findFirst<T extends FindArgs<Schema, Model, true>>(
+    useFindFirst<T extends FindArgs<Schema, Model, true>>(
         args?: SelectSubset<T, FindArgs<Schema, Model, true>>,
         options?: Omit<UseQueryOptions<ModelResult<Schema, Model, T>[]>, 'queryKey'>,
     ): UseQueryResult<ModelResult<Schema, Model, T> | null>;
 
-    create<T extends CreateArgs<Schema, Model>>(
+    useCreate<T extends CreateArgs<Schema, Model>>(
         options?: UseMutationOptions<ModelResult<Schema, Model, T>, DefaultError, T>,
     ): UseMutationResult<ModelResult<Schema, Model, T>, DefaultError, T>;
 };
 
-function uncapitalize(s: string) {
-    return s.charAt(0).toLowerCase() + s.slice(1);
-}
-
-export function toHooks<Schema extends SchemaDef>(schema: Schema): toHooks<Schema> {
+export function useQueryHooks<Schema extends SchemaDef>(schema: Schema): QueryHooks<Schema> {
     return Object.entries(schema.models).reduce(
         (acc, [model, _]) =>
             Object.assign(acc, {
-                [uncapitalize(model)]: toModelHooks(schema, model as GetModels<Schema>),
+                [lowerCaseFirst(model)]: toModelHooks(schema, model as GetModels<Schema>),
             }),
-        {} as toHooks<Schema>,
+        {} as QueryHooks<Schema>,
     );
 }
 
-function toModelHooks<Schema extends SchemaDef, Model extends GetModels<Schema>>(schema: Schema, model: Model): any {
+function toModelHooks<Schema extends SchemaDef, Model extends GetModels<Schema>>(
+    schema: Schema,
+    model: Model,
+): ModelQueryHooks<Schema, Model> {
     const modelDef = schema.models[model];
     if (!modelDef) {
         throw new Error(`Model ${model} not found in schema`);
     }
 
     return {
-        findMany: () => {
+        useFindMany: () => {
             return {
                 data: [],
                 isLoading: false,

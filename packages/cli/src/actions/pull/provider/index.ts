df8371de44806bdda882d4109e4137caff30897b
Román Benjámin <svetch.game@gmail.com>
2025-11-12 21:43:46 +0100

    fix: catch up with feature updates
    - improve code styling
    - enable schema support for db pull

--- packages/cli/src/actions/pull/provider/index.ts
+++ packages/cli/src/actions/pull/provider/index.ts
@@ -1,9 +1,11 @@
+import type { DataSourceProviderType } from '@zenstackhq/schema';
 export * from './provider';
 
 import { postgresql } from './postgresql';
+import type { IntrospectionProvider } from './provider';
 import { sqlite } from './sqlite';
 
-export const providers = {
+export const providers: Record<DataSourceProviderType, IntrospectionProvider> = {
     postgresql,
     sqlite,
 };

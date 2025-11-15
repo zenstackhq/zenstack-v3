ad1a5000c471807e05ba0f631ba44892a570a89c
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-11-15 09:24:56 -0800

    refactor(orm): consolidate kysely dialects under /dialects exports

--- packages/dialects/sql.js/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import config from '@zenstackhq/eslint-config/base.js';
-
-/** @type {import("eslint").Linter.Config} */
-export default config;
---
+++ packages/orm/src/dialects/sqlite.ts
@@ -0,0 +1,1 @@
+export { SqliteDialect, type SqliteDialectConfig } from 'kysely';

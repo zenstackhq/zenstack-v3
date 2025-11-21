a2e9858fe0a676e066bbe767ef45cbeb5fabe7d3
Román Benjámin <svetch.game@gmail.com>
2025-11-21 21:40:41 +0100

    test: run db pull e2e test also for postgres

--- packages/cli/test/utils.ts
+++ packages/cli/test/utils.ts
@@ -1,14 +1,52 @@
-import { createTestProject } from '@zenstackhq/testtools';
+import { createTestProject, getTestDbProvider } from '@zenstackhq/testtools';
+import { createHash } from 'node:crypto';
 import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
+import { expect } from 'vitest';
 
-const ZMODEL_PRELUDE = `datasource db {
-    provider = "sqlite"
-    url      = "file:./dev.db"
-}`;
+const TEST_PG_CONFIG = {
+    host: process.env['TEST_PG_HOST'] ?? 'localhost',
+    port: process.env['TEST_PG_PORT'] ? parseInt(process.env['TEST_PG_PORT']) : 5432,
+    user: process.env['TEST_PG_USER'] ?? 'postgres',
+    password: process.env['TEST_PG_PASSWORD'] ?? 'postgres',
+};
+
+function getTestDbName(provider: string) {
+    if (provider === 'sqlite') {
+        return './test.db';
+    }
+    const testName = expect.getState().currentTestName ?? 'unnamed';
+    const testPath = expect.getState().testPath ?? '';
+    // digest test name
+    const digest = createHash('md5')
+        .update(testName + testPath)
+        .digest('hex');
+    // compute a database name based on test name
+    return (
+        'test_' +
+        testName
+            .toLowerCase()
+            .replace(/[^a-z0-9_]/g, '_')
+            .replace(/_+/g, '_')
+            .substring(0, 30) +
+        digest.slice(0, 6)
+    );
+}
 
 export function createProject(zmodel: string, addPrelude = true) {
+    const provider = getTestDbProvider() ?? 'sqlite';
+    const dbName = getTestDbName(provider);
+    const dbUrl =
+        provider === 'sqlite'
+            ? `file:${dbName}`
+            : `postgres://${TEST_PG_CONFIG.user}:${TEST_PG_CONFIG.password}@${TEST_PG_CONFIG.host}:${TEST_PG_CONFIG.port}/${dbName}`;
+
+    const ZMODEL_PRELUDE = `datasource db {
+    provider = "${provider}"
+    url      = "${dbUrl}"
+}
+`;
     const workDir = createTestProject();
     fs.mkdirSync(path.join(workDir, 'zenstack'), { recursive: true });
     const schemaPath = path.join(workDir, 'zenstack/schema.zmodel');

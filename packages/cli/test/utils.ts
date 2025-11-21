73d67b68e0a2692116c4446c33430a2a18ee6abb
Román Benjámin <svetch.game@gmail.com>
2025-11-21 21:19:35 +0100

    fix: formatting for e2e test schemas

--- packages/cli/test/utils.ts
+++ packages/cli/test/utils.ts
@@ -5,15 +5,14 @@ import path from 'node:path';
 
 const ZMODEL_PRELUDE = `datasource db {
     provider = "sqlite"
-    url = "file:./dev.db"
-}
-`;
+    url      = "file:./dev.db"
+}`;
 
 export function createProject(zmodel: string, addPrelude = true) {
     const workDir = createTestProject();
     fs.mkdirSync(path.join(workDir, 'zenstack'), { recursive: true });
     const schemaPath = path.join(workDir, 'zenstack/schema.zmodel');
-    fs.writeFileSync(schemaPath, addPrelude ? `${ZMODEL_PRELUDE}\n\n${zmodel}` : zmodel);
+    fs.writeFileSync(schemaPath, addPrelude ? `${ZMODEL_PRELUDE}\n${zmodel}` : zmodel);
     return workDir;
 }
 

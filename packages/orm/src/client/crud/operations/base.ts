37c3182307e0b9d4cadd0615e770aa637d978173
sanny-io <sannysherief@gmail.com>
2025-12-17 06:40:03 +0000

    Use `replaceAll` instead.

--- packages/orm/src/client/crud/operations/base.ts
+++ packages/orm/src/client/crud/operations/base.ts
@@ -915,8 +915,7 @@ export abstract class BaseOperationHandler<Schema extends SchemaDef> {
         invariant(typeof format === 'string', 'generated identifier format value must be a string');
         invariant(format.includes('%s'), 'generated identifier format strings must include "%s"');
 
-        // replaceAll instead? Does anyone even need that...?
-        return format.replace('%s', generated);
+        return format.replaceAll('%s', generated);
     }
 
     protected async update(

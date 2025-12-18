47ee28462d0b9897399a1fba25357d239f8589b5
sanny-io <sannysherief@gmail.com>
2025-12-18 18:09:52 +0000

    Add language support and tests.

---
+++ packages/language/test/function-invocation.test.ts
@@ -0,0 +1,209 @@
+import { describe, it } from 'vitest';
+import { loadSchema, loadSchemaWithError } from './utils';
+
+describe('Function Invocation Tests', () => {
+    it('id functions should not require format strings', async () => {
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(uuid())
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(uuid(7))
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(nanoid())
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(nanoid(8))
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(ulid())
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(cuid())
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(cuid(2))
+            }  
+        `,
+        );
+    });
+
+    it('id functions should allow valid format strings', async () => {
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(uuid(7, '%s_user'))
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(cuid(2, '%s'))
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(ulid('user_%s'))
+            }  
+        `,
+        );
+
+        await loadSchema(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(nanoid(8, 'user_%s'))
+            }  
+        `,
+        );
+    });
+
+    it('id functions should reject invalid format strings', async () => {
+        await loadSchemaWithError(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(uuid(7, 'user_%'))
+            }  
+        `,
+            'argument must include',
+        );
+
+        await loadSchemaWithError(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(nanoid(8, 'user'))
+            }  
+        `,
+            'argument must include',
+        );
+
+        await loadSchemaWithError(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(ulid('user_%'))
+            }  
+        `,
+            'argument must include',
+        );
+
+        await loadSchemaWithError(
+            `
+            datasource db {
+                provider = 'sqlite'
+                url      = 'file:./dev.db'
+            }
+
+            model User {
+                id String @id @default(cuid(2, 'user_%'))
+            }  
+        `,
+            'argument must include',
+        );
+    });
+});

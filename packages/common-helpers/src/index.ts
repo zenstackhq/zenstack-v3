f969ec4ef04525a5ec6e3efacb0381e536d89146
FTB_lag <tabolskyy.git@gmail.com>
2026-01-07 04:40:38 +0300

    feat(common-helpers): implement single-debounce

--- packages/common-helpers/src/index.ts
+++ packages/common-helpers/src/index.ts
@@ -4,6 +4,7 @@ export * from './is-plain-object';
 export * from './lower-case-first';
 export * from './param-case';
 export * from './safe-json-stringify';
+export * from './single-debounce';
 export * from './sleep';
 export * from './tiny-invariant';
 export * from './upper-case-first';

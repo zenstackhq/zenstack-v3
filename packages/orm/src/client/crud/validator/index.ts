3fde86d9cfc8866daa67ff07b636b8c993163117
FTB_lag <tabolskyy.git@gmail.com>
2026-01-07 16:52:50 +0300

    feat(cli): implement watch mode for generate (#554)
    * feat(cli): implement watch mode for generate
    
    * chore(root): update pnpm-lock.yaml
    
    * chore(cli): track all model declaration and removed paths, logs in past tense
    
    * fix(cli): typo, unused double array from
    
    * fix(orm): preserve zod validation errors when validating custom json types
    
    * update
    
    * chore(cli): move import, fix parallel generation on watch
    
    * feat(common-helpers): implement single-debounce
    
    * chore(cli): use single-debounce for debouncing
    
    * feat(common-helpers): implement single-debounce
    
    * fix(common-helpers): re run single-debounce
    
    ---------
    
    Co-authored-by: Yiming Cao <yiming@whimslab.io>
    Co-authored-by: ymc9 <104139426+ymc9@users.noreply.github.com>

--- packages/orm/src/client/crud/validator/index.ts
+++ packages/orm/src/client/crud/validator/index.ts
@@ -402,9 +402,13 @@ export class InputValidator<Schema extends SchemaDef> {
         // zod doesn't preserve object field order after parsing, here we use a
         // validation-only custom schema and use the original data if parsing
         // is successful
-        const finalSchema = z.custom((v) => {
-            return schema.safeParse(v).success;
+        const finalSchema = z.any().superRefine((value, ctx) => {
+            const parseResult = schema.safeParse(value);
+            if (!parseResult.success) {
+                parseResult.error.issues.forEach((issue) => ctx.addIssue(issue as any));
+            }
         });
+
         this.setSchemaCache(key!, finalSchema);
         return finalSchema;
     }
@@ -515,7 +519,7 @@ export class InputValidator<Schema extends SchemaDef> {
         }
 
         // expression builder
-        fields['$expr'] = z.custom((v) => typeof v === 'function').optional();
+        fields['$expr'] = z.custom((v) => typeof v === 'function', { error: '"$expr" must be a function' }).optional();
 
         // logical operators
         fields['AND'] = this.orArray(

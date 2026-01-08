94317bdd2e854c157aaa15911536f724aa09ce6b
ymc9 <104139426+ymc9@users.noreply.github.com>
2026-01-08 20:38:32 +0800

    addressing PR comments

--- packages/orm/src/client/result-processor.ts
+++ packages/orm/src/client/result-processor.ts
@@ -93,7 +93,7 @@ export class ResultProcessor<Schema extends SchemaDef> {
     private processFieldValue(value: unknown, fieldDef: FieldDef) {
         const type = fieldDef.type as BuiltinType;
         if (Array.isArray(value)) {
-            value.forEach((v, i) => (value[i] = this.dialect.transformOutput(v, type, !!fieldDef.array)));
+            value.forEach((v, i) => (value[i] = this.dialect.transformOutput(v, type, false)));
             return value;
         } else {
             return this.dialect.transformOutput(value, type, !!fieldDef.array);
